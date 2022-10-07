dc01818f2fd318a512092440d8cf495fdaf39564
GottaGinger <sierratmorris@gmail.com>
2022-10-07 18:24:39 -0400

    UpdatedtoFollow

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,27 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        if (Math.random() < 0.5){
-            System.out.println("Heads");
-        }else{
-            System.out.println("Tails");
-        }
+        Scanner input = new Scanner(System.in);
+        System.out.println("Guess Heads or Tails");
+        String headsOrTailsGuess = input.nextLine();
+        System.out.println("Number of Flips");
+        double numberOfFlips = input.nextInt();
+        double correctCount = 0;
+
+       for(int i=0; i< numberOfFlips; i++) {
+           if (Math.random() < 0.5){
+               System.out.println("heads");
+               if(headsOrTailsGuess.equals("Heads") || headsOrTailsGuess.equals("heads"))
+                   correctCount++;
+           } else{
+               System.out.println("tails");
+               if(headsOrTailsGuess.equals("Tails") || headsOrTailsGuess.equals("tails"))
+                   correctCount++;
+           }
+         }
+        System.out.println(correctCount);
+       System.out.println(correctCount / numberOfFlips * 100);
 
     }
 }
